8a0b4fa035a9f817dffe441da3fb8c56d96b70d9
Jayadev J Nair <138727186+jayadev05@users.noreply.github.com>
2025-02-04 21:07:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react';
 
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(
+    {
+      babel:{
+        plugins :[["react-compiler"]],
+      },
+    }
+  )],
   resolve: {
     alias: {
       '@': '/src', 
